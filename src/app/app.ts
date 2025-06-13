71af7b4ebb2664fcf90dd8cae2dd18610b97d840
Vinicius Santos <vinicius.santos@essentia.com.br>
2025-06-13 12:04:06 -0300

    button primary e secundary

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { PrimaryButton } from './components/primary-button/primary-button';
+import { SecundaryButton } from './components/secundary-button/secundary-button';
+SecundaryButton
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, PrimaryButton, SecundaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/item-certificado/item-certificado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-certificado',
+  imports: [],
+  templateUrl: './item-certificado.html',
+  styleUrl: './item-certificado.css'
+})
+export class ItemCertificado {
+
+}
